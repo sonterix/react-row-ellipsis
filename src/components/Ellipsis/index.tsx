35022a3e8b14108ffe0dee6122d488fc5294d889
Nick Bukovskiy <sonterixo@gmail.com>
2022-03-09 01:26:01 +0200

    Base version added

--- src/components/Ellipsis/index.tsx
+++ src/components/Ellipsis/index.tsx
@@ -1,5 +1,22 @@
-const Ellipsis = (): JSX.Element => {
-  return <div>Test</div>
+type EllipsisProps = {
+  [key: string]: any
+  lines: number
+  text: string
 }
+
+const Ellipsis = ({ lines, text, ...props }: EllipsisProps): JSX.Element => (
+  <p
+    style={{
+      display: '-webkit-box',
+      WebkitLineClamp: lines,
+      WebkitBoxOrient: 'vertical',
+      overflow: 'hidden',
+      textOverflow: 'ellipsis'
+    }}
+    {...props}
+  >
+    {text}
+  </p>
+)
 
 export default Ellipsis
