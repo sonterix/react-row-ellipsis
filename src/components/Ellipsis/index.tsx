3763a6ec86f8913668273cb5b62f7382ae8e7547
Nick Bukovskyi <sonterixo@gmail.com>
2022-04-19 23:43:10 +0300

    Update typization

--- src/components/Ellipsis/index.tsx
+++ src/components/Ellipsis/index.tsx
@@ -1,23 +1,24 @@
-type EllipsisProps = {
-  [key: string]: any
-  lines?: number
+interface EllipsisProps extends React.HTMLProps<HTMLParagraphElement> {
   text: string
+  lines?: number
 }
 
-const Ellipsis = ({ lines, text, ...props }: EllipsisProps): JSX.Element => (
-  <p
-    style={{
-      display: '-webkit-box',
-      WebkitLineClamp: lines,
-      WebkitBoxOrient: 'vertical',
-      overflow: 'hidden',
-      textOverflow: 'ellipsis'
-    }}
-    {...props}
-  >
-    {text}
-  </p>
-)
+const Ellipsis = ({ lines, text, style, ...props }: EllipsisProps): JSX.Element => {
+  const customStyles: React.CSSProperties = {
+    ...(style || {}),
+    display: '-webkit-box',
+    WebkitLineClamp: lines,
+    WebkitBoxOrient: 'vertical',
+    overflow: 'hidden',
+    textOverflow: 'ellipsis'
+  }
+
+  return (
+    <p style={customStyles} {...props}>
+      {text}
+    </p>
+  )
+}
 
 Ellipsis.defaultProps = {
   lines: 1
